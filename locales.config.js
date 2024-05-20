242992058573b1bcc39d7fb92b2ed4625b9a571f
ziegfiroyt <sozenugur@gmail.com>
2024-05-20 10:58:36 +0300

    chore: locales.config

--- locales.config.js
+++ locales.config.js
@@ -14,8 +14,7 @@ module.exports = [
     heroImages: {
       dark: "/locales/tr/images/bg/d-bg.webp",
       light: "/locales/tr/images/bg/bg.webp",
-      darkMobile:
-        "/locales/tr/images/bg/d-bg-m.webp",
+      darkMobile: "/locales/tr/images/bg/d-bg-m.webp",
       lightMobile: "/locales/tr/images/bg/bg-m.webp",
     },
     createPasswordEmailParams: {
@@ -40,10 +39,9 @@ module.exports = [
       height: "14px",
     },
     heroImages: {
-      dark: "https://images.unsplash.com/photo-1507237081139-5dfb209dba79?q=92&w=1436&h=900&auto=format&fit=crop",
+      dark: "/locales/en/images/bg/d-bg.webp",
       light: "/locales/en/images/bg/bg.webp",
-      darkMobile:
-        "https://images.unsplash.com/photo-1509484710660-55e53f20a84b?q=95&w=502&h=1000&auto=format&fit=crop",
+      darkMobile: "/locales/en/images/bg/d-bg-m.webp",
       lightMobile: "/locales/en/images/bg/bg-m.webp",
     },
     createPasswordEmailParams: {
